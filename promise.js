02f53301960a37d26a7236ed2c96a891a3a9496e
ft <fengtong@mail.ustc.edu.cn>
2014-03-01 16:58:05 +0800

    first

---
+++ promise.js
@@ -0,0 +1,87 @@
+var p = Promise
+!function() {
+
+  function Promise(resolver) {
+    var queue = []
+    resolver(resolve, reject)
+
+    function next(i, val) {
+      setTimeout(function() { // dirty but out queue(resolve, reject) must wait then inqueue
+        while (queue.length) {
+          var arr = queue.shift()
+          if (typeof arr[i] === 'function') {
+            try {
+              var chain = arr[i](val)
+            } catch (e) {
+              return reject(e)
+            }
+            if (chain && typeof chain.then === 'function') {
+              return chain.then(resolve, reject)
+            } else {
+              return Promise.resolved(chain).then(resolve, reject)
+            }
+          }
+        }
+      })
+    }
+
+    function resolve(x) {
+      next(0, x)
+    }
+
+    function reject(reason) {
+      next(1, reason)
+    }
+
+    this.chain = this.then = function(resolve, reject) {
+      queue.push([resolve, reject])
+      return this
+    }
+
+    this.catch = function(reject) {
+      return this.then(undefined, reject)
+    }
+
+  }
+
+  Promise.resolved = Promise.cast = function(x) {
+    return new Promise(function(resolve) {
+      resolve(x)
+    })
+  }
+
+  Promise.rejected = function(reason) {
+    return new Promise(function(resolve, reject) {
+      reject(reason)
+    })
+  }
+
+  Promise.all = function(values) {
+    var defer = Promise.deferred()
+    var len = values.length
+    var results = []
+    values.forEach(function(p, i) {
+      p.then(function(x) {
+        results[i] = x
+        len--
+        if (len === 0) {
+          defer.resolve(results)
+        }
+      }, function(r) {
+        defer.reject(r)
+      })
+    })
+    return defer.promise
+  }
+
+  Promise.deferred = function() {
+    var result = {}
+    result.promise = new Promise(function(resolve, reject) {
+      result.resolve = resolve
+      result.reject = reject
+    })
+    return result
+  }
+
+  window.Promise = Promise
+}()
---
+++ readme.md
@@ -0,0 +1,30 @@
+Promise.js
+===
+
+`promise.js` is a extreme simple javascript promise lib
+
+demo (todo)
+
+
+### API
+
+- Promise.referred
+
+- Promise.resolved
+
+- Promise.rejected
+
+- Promise.all
+
+and
+
+- promise.then
+
+- promise.chain
+
+- promise.catch
+
+
+### Notes
+
+not be tested yet
